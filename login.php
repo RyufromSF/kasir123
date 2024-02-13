e549f31c0e0c1ea1bee6fb16ac28da8ce260d2d1
RyufromSF <notryuto@gmail.com>
2024-02-13 12:52:54 +0700

    connect

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form action="validasi.php" method="post">
+        <h1>Selamat Datang!</h1>
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" name="username"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td>
+                    <button type="submit">LOGIN</button>
+                    <button type="reset">CLEAR</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
